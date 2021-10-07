c69e237d59594fc48e2b713cf0c2773a9b2b5a72
Héctor Zamudio <47469444+zamudio1243@users.noreply.github.com>
2021-10-06 21:24:23 -0500

    :sparkles: Ejemplo de elemtos basicos compose

--- app/src/main/java/com/example/compose_simple_example/MainActivity.kt
+++ app/src/main/java/com/example/compose_simple_example/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.compose_simple_example
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose_simple_example.data.Message
 import com.example.compose_simple_example.ui.theme.ComposesimpleexampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +27,54 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposesimpleexampleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MessageCard(
+                        msg = Message("Android", "Jetpack Compose")
+                    )
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message){
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.he),
+            contentDescription = "he",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column() {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp){
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+
+            }
+
+        }
+
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    ComposesimpleexampleTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard(){
+    MessageCard(
+        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
---
+++ app/src/main/java/com/example/compose_simple_example/data/Message.kt
@@ -0,0 +1,6 @@
+package com.example.compose_simple_example.data
+
+data class Message(
+    val author: String = "",
+    val body: String = ""
+)
