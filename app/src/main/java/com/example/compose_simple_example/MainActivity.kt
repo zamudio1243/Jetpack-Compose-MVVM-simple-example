16d61c2566162e596627629876c5b912ba434ab9
Héctor Zamudio <47469444+zamudio1243@users.noreply.github.com>
2021-10-07 23:11:11 -0500

    :sparkles: animaciones básicas a los mensajes

--- app/src/main/java/com/example/compose_simple_example/MainActivity.kt
+++ app/src/main/java/com/example/compose_simple_example/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.compose_simple_example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose_simple_example.data.Gender
 import com.example.compose_simple_example.data.Message
 import com.example.compose_simple_example.ui.components.MessageCard
 import com.example.compose_simple_example.ui.theme.ComposeSimpleExampleTheme
@@ -16,13 +21,54 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeSimpleExampleTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    MessageCard(msg = Message(author = "Jetpack", body = "Compose"))
+                    Conversation(populateList())
                 }
             }
         }
     }
 }
 
+@Composable
+fun Conversation(messages: ArrayList<Message>){
+    LazyColumn{
+        items(messages){ message ->
+            MessageCard(msg = message)
+        }
+    }
 
+}
 
+@Preview
+@Composable
+fun PreviewConversation(){
+    ComposeSimpleExampleTheme{
+        Conversation(messages = populateList())
+    }
+}
+
+private fun populateList(): ArrayList<Message> {
+    val userDefaultList = ArrayList<Message>()
+    userDefaultList.add(
+        Message(
+            author = "Alfredo",
+            body = "Hola ¿cómo estan?",
+            gender = Gender.MALE
+        )
+    )
+    userDefaultList.add(
+        Message(
+            author = "Carlos",
+            body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec dolor at tellus consequat posuere in et massa. Etiam eu sollicitudin ante. ",
+            gender = Gender.MALE
+        )
+    )
+    userDefaultList.add(
+        Message(
+            author = "Isabel",
+            body = "Holaaaaaa",
+            gender = Gender.FEMALE
+        )
+    )
+    return  userDefaultList
+}
 
--- app/src/main/java/com/example/compose_simple_example/ui/components/MessageCard.kt
+++ app/src/main/java/com/example/compose_simple_example/ui/components/MessageCard.kt
@@ -1,15 +1,19 @@
 package com.example.compose_simple_example.ui.components
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,32 +23,59 @@ import com.example.compose_simple_example.data.Gender
 
 @Composable
 fun MessageCard(msg: Message){
+    // Agrega padding al rededor del mensaje
     Row(modifier = Modifier.padding(all = 8.dp)) {
-        val profilePic = when(msg.gender){
-            Gender.MALE -> painterResource(id = R.drawable.he)
-            Gender.FEMALE -> painterResource(id = R.drawable.she)
-            Gender.UNKNOWN -> painterResource(id = R.drawable.she)
-        }
         Image(
-            painter = profilePic,
+            painter = when(msg.gender){
+                Gender.MALE -> painterResource(id = R.drawable.he)
+                Gender.FEMALE -> painterResource(id = R.drawable.she)
+                Gender.UNKNOWN -> painterResource(id = R.drawable.she)
+            },
             contentDescription = "gender",
             modifier = Modifier
+                // Coloca el tamaño de la imagen a 40 dp
                 .size(40.dp)
+                // Le da forma de circulo a la imagen
                 .clip(CircleShape)
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
+        // Añade espacio horizontal entre la imagen y la columna
         Spacer(modifier = Modifier.width(8.dp))
-        Column() {
+
+        // Se mantiene el estado de si es mensaje esta expandido o no
+        // En esta variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+
+        // surfaceColor se actualizará gradualmente de un color a otro
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        // Alterna la variable isExpanded cuando hacemos click en esta Columna
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
-                color = MaterialTheme.colors.secondaryVariant,
-                style = MaterialTheme.typography.subtitle2
+                style = MaterialTheme.typography.subtitle2,
+                color = MaterialTheme.colors.secondaryVariant
             )
+
+            // Añade espacio horizontal entre la autor y el mensaje
             Spacer(modifier = Modifier.width(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp){
+
+            Surface(
+                shape = MaterialTheme.shapes.medium, elevation = 1.dp,
+                // Este color irá cambiante gradualmente de color primario a surface
+                color = surfaceColor,
+                // animateContentSize cambiará el tamaño del Surface gradualmente
+                modifier = Modifier.animateContentSize().padding(1.dp),
+            ){
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // Si el mensaje está expandido, se muestra el contenido completo
+                    // de otra forma solo se mostrará la primera linea
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
