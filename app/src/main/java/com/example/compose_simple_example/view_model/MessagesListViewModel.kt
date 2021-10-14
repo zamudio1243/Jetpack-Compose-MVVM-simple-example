b2465d6be7a938d21470dc204f21d4c22747fdfc
Héctor Zamudio <47469444+zamudio1243@users.noreply.github.com>
2021-10-13 19:16:32 -0500

    :sparkles: Se agregó el view model a la actividad

--- app/src/main/java/com/example/compose_simple_example/view_model/MessagesListViewModel.kt
+++ app/src/main/java/com/example/compose_simple_example/view_model/MessagesListViewModel.kt
@@ -1,20 +1,32 @@
 package com.example.compose_simple_example.view_model
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import com.example.compose_simple_example.data.Gender
 import com.example.compose_simple_example.data.Message
 
-class MessagesListViewModel(): ViewModel() {
-    val messagesList = mutableListOf<Message>()
+/**
+ * ViewModel de la lista de mensajes
+ */
+class MessagesListViewModel: ViewModel() {
+    val messagesList = mutableStateListOf<Message>()
 
     init {
         messagesList.addAll(populateList())
     }
 
+    /**
+     * Agrega un mensaje a la lista de mensajes
+     * @param message Mensaje a agregar a lista
+     */
     fun addMessage(message: Message){
         messagesList.add(message)
     }
 
+    /**
+     * Agrega una lista inicial de mensajes al ViewModel
+     * @return ArrayList de mensajes
+     */
     private fun populateList() = arrayListOf(
         Message(
             author = "Alfredo",
